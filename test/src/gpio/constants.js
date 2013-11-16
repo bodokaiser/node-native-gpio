bcfe1455ae55c53000e35006f3ac41f3ac28f3e5
Bodo Kaiser <i@bodokaiser.io>
2013-11-16 13:49:32 +0100

    moved constant tests into GPIO root namespace

--- test/src/gpio/constants.js
+++ test/src/gpio/constants.js
@@ -2,36 +2,20 @@ var should = require('should');
 
 module.exports = function(GPIO) {
 
-    describe('IN', function() {
-
-        it('should equal "0"', function() {
-            GPIO.IN.should.equal(0);
-        });
-
+    it('IN should equal "0"', function() {
+        GPIO.IN.should.equal(0);
     });
 
-    describe('OUT', function() {
-
-        it('should equal "1"', function() {
-            GPIO.OUT.should.equal(1);
-        });
-
+    it('OUT should equal "1"', function() {
+        GPIO.OUT.should.equal(1);
     });
 
-    describe('LOW', function() {
-
-        it('should equal "0"', function() {
-            GPIO.LOW.should.equal(0);
-        });
-
+    it('LOW should equal "0"', function() {
+        GPIO.LOW.should.equal(0);
     });
 
-    describe('HIGH', function() {
-
-        it('should equal "1"', function() {
-            GPIO.HIGH.should.equal(1);
-        });
-
+    it('HIGH should equal "1"', function() {
+        GPIO.HIGH.should.equal(1);
     });
 
 };
